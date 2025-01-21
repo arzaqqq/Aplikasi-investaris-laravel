bedc370ea53f2b09856b3c8fe2f6c6932803f552
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-01-21 09:13:16 +0700

    [ADD] Membuat validasi pada form login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -28,31 +28,37 @@
         @csrf
         @method('POST')
         <div class="input-group mb-3">
-          <input type="email" name="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}">
+            @error('email')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-envelope"></span>
+                </div>
             </div>
-          </div>
         </div>
+
         <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
+            @error('password')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                </div>
             </div>
-          </div>
         </div>
+
         <div class="row">
-          <div class="col-8">
-          </div>
-          <!-- /.col -->
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
+            <div class="col-8">
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            </div>
         </div>
-      </form>
+    </form>
 
 
       <!-- /.social-auth-links -->
